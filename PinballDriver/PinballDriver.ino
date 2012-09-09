2f40c9b5fa8550006066963d9ad5766d4210b619
Max <max@boinx.com>
2012-09-09 22:52:56 +0200

    Control Game Relay

--- PinballDriver/PinballDriver.ino
+++ PinballDriver/PinballDriver.ino
@@ -25,6 +25,42 @@
   20 Ground
 */
 
+/*
+  SF2 Solenoids
+  00  0 0  0  Pop Bumber
+  01  0 1  0  Left Kicking Rubber
+  02  0 2  0  Right Kicking Rubber
+  03  0 3  0  Bottom Left Kicker
+  04  0 4  0  Bottom Right Kicker
+  05  0 5  0  Left Plunger Gate
+  06  0 6  0  Right Plunger Gate
+  07  0 7  1  Left Stargate Ramp
+  08  1 0  1  Right Stargate Ramp
+  09  1 1  0  Left Hole
+  10  1 2  0  Right Hole
+  11  1 3  0  - Unused
+  12  1 4  0  - Unused
+  13  1 5  0  Car Reset
+  14  1 6  0  Lamp Left Tap 
+  15  1 7  0  Lamp Lightstrip
+  16  2 0  0  Lamp Left Bottom
+  17  2 1  0  Lamp Ramp
+  18  2 2  0  Lamp Left Flipper
+  19  2 3  0  Lamp Right Bottom
+  20  2 4  0  Lamp Right Center
+  21  2 5  0  Lamp Right Top
+  22  2 6  0  Lamp Right Flipper
+  23  2 7  1  Low Playfield Flipper Relay
+  24  3 0  1  Motor Relay
+  25  3 1  1  Lightbox Relay 
+  26  3 2  0  Coin Meter
+  27  3 3  0  Ball Release
+  28  3 4  0  Outhole
+  29  3 5  0  Knocker
+  30  3 6  1  Tilt Relay
+  31  3 7  1  Game Over Relay
+*/
+
 // 0, 1, 2, 3, 5, 6, 7, 8, 9
 
 // 0 Master Reset
@@ -41,15 +77,23 @@
 
 
 
-byte solenoidData[4]; // 32 solenoid values
+byte solenoidData[4] = {
+  0x00, 0x00, 0x00, 0x00
+}; // 32 solenoid values
+
+// after each run the solenoidData & solenoidReset
+// this will ensure that solenoids are not triggered for a long time
+byte solenoidHold[4] = {
+  0x80, 0x01, 0x80, 0xC3
+};
 
 const int masterResetPin = 0;
 
 const int solenoidStrobePins[4] = {
+  -1,
+  -1,
+  -1,
   6,
-  -1,
-  -1,
-  -1,
 };
 
 const int solenoidDataPins[8] = {
@@ -82,27 +126,11 @@ void setup() {
   
   digitalWrite(masterResetPin, HIGH);
   delay(2000);
+  
+  solenoidData[3] = 0x80;
 }
 
-long oldMillis = 0;
-
-bool on = false;
-long onDelay = 4000;
-
 void loop() {
-  long delta = millis() - oldMillis;
-  oldMillis = millis();
-  
-  onDelay -= delta;
-  if(onDelay < 0) {
-    onDelay += 4000;
-    
-    on = !on;
-    
-    solenoidData[0] = on ? (1 << 7) : (0 << 7);
-  }
-
-  
   for(int solenoidStrobeIndex = 0; solenoidStrobeIndex < 4; ++solenoidStrobeIndex) {    
     const byte solenoidByte = solenoidData[solenoidStrobeIndex];
     
@@ -138,5 +166,8 @@ void loop() {
 //    delay(3000);
   }
   
+  for(int solenoidIndex = 0; solenoidIndex < 4; ++solenoidIndex) {
+    solenoidData[solenoidIndex] = solenoidData[solenoidIndex] & solenoidHold[solenoidIndex];
+  }
 }
 
